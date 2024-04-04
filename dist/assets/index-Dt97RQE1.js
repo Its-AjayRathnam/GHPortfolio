634f3573abf18f0df47b65db8eb79782d55af75d
Ajay Rathnam <its.ajayrathnam@gmail.com>
2024-04-04 21:54:22 +0530

    Test

---
+++ dist/assets/index-Dt97RQE1.js
@@ -0,0 +1,1 @@
+import{e as o,f as n,h as r}from"./index-DBD5nJH-.js";const _={__name:"index",setup(t){return(c,l)=>{const e=o("HelloWorld");return n(),r(e)}}};export{_ as default};
---
+++ src/components/Tabler/tabler-icons.js
@@ -0,0 +1,16 @@
+
+import { IconHome, IconCamera, IconHeart, IconBrightness, IconSun, IconMoon, IconMenu2, IconSunHigh, IconArrowUp /* ...other icons */ } from '@tabler/icons-vue';
+
+export const tabler = {
+  IconHome,
+  IconCamera,
+  IconHeart,
+  IconBrightness,
+  IconSun,
+  IconMoon,
+  IconMenu2,
+  IconSunHigh,
+  IconArrowUp
+  // ...add other icons here
+};
+
